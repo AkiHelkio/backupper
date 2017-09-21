4457d1f2db7869d49a559d21f8b93fc0c1d41425
AkiHelkio <aki.helkio@gmail.com>
2017-09-21 18:11:52 +0300

    Added subclasses and paramiko for connections

--- client.py
+++ client.py
@@ -1,16 +1,75 @@
 #! -*- coding:utf8 -*-
 
 
-class Client:
-    def __init__(self, config):
-        self.config = config
+import os
+import sys
+import json
+import paramiko
+from getpass import getpass
+from datetime import datetime
+
+
+class Log:
+    def __init__(self):
+        self.textformat = "[{time}][{type}] {text}"
+        self.eventtype = "info"
+        
+    def event(self, text, eventtype=None):
+        if (eventtype):
+            self.eventtype = eventtype
+        row = {
+            "time" = datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "type" = self.eventtype,
+            "text" = text
+        }
+        print(self.textformat.format(**row))
+
+
+# basic json to dict to attributes reader
+class Configreader:
+    def __init__(self, configpath):
+        self.configpath = configpath
+        self.config = None
+        self.load()
+        
+    def load(self):
+        try:
+            with open(self.configpath, 'r') as f:
+                self.config = json.load(f)
+            # convert dict to attributes
+            for k,v in self.config.items():
+                setattr(self, k, v)
+        except FileNotFoundException:
+            print("Unable to find", self.configpath)
+            sys.exit(1)
+
+
+# client extends Configreader
+class Client(Configreader):
+    def __init__(self, configpath):
+        super().__init__(configpath)
+        # paramiko variables
+        self.transport = None
+        self.sftp = None
 
     def connect(self):
-        pass
+        try:
+            # get key
+            key = paramiko.RSAKey.from_private_key_file(self.keypath)
+            # make connection
+            self.transport = paramiko.Transport((self.host, self.port))
+            self.transport.connect(username=self.username, pkey=key)
+            print("Connected to", self.host, "as", self.username)
+        except Exception as e:
+            sys.exit(e.args)
+            
 
     def disconnect(self):
-        pass
-
+        self.sftp.close()
+        self.log.event('Sftp session closed')
+        self.transport.close()
+        self.log.event('Transport closed')
+        
     def backup(self, files):
         pass
 
@@ -19,7 +78,8 @@ class Client:
 
 
 def main():
-    print("")
+    client = Client('config.json')
+    client.
 
 
 if __name__ == '__main__':
