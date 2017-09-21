596dc4f351cb1d16af3aeb1aabd10a9e3c507e71
AkiHelkio <aki.helkio@gmail.com>
2017-09-21 16:47:45 +0300

    added client.py, first commit

---
+++ client.py
@@ -0,0 +1,27 @@
+#! -*- coding:utf8 -*-
+
+
+class Client:
+    def __init__(self, config):
+        self.config = config
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def backup(self, files):
+        pass
+
+    def retrieve(self, backup):
+        pass
+
+
+def main():
+    print("")
+
+
+if __name__ == '__main__':
+    main()
+
